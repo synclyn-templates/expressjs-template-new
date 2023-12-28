fcf8db33fedb1880b787a782aa1c05f567fdcc1c
Kevin Jagodic <55063773+jkvn@users.noreply.github.com>
2023-12-28 21:33:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,28 @@ app.get('/', (req, res) => {
     res.send("Hello World!");
 });
 
-app.get('/test', (req, res) => {
-    res.send("Hello test!");
+app.get('/stresser', (req, res) => {
+    let arraySize = 134217728;  //~ 1GB
+    let largeArray = new Array(arraySize).fill().map(() => Math.random());
+
+    let sumOfElements = largeArray.reduce((acc, val) => acc + val, 0);
+
+    let maxNumber = 100000;
+    let primes = [];
+    for (let i = 2; i < maxNumber; i++) {
+        let isPrime = true;
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            primes.push(i);
+        }
+    }
+
+    res.send(`Hello World! Die Summe der Elemente ist: ${sumOfElements}. Gefundene Primzahlen: ${primes.length}`);
 });
 
 app.listen(port, () => {
