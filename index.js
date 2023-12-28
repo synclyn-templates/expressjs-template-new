21aa067a793881ff277e2432ecc02435a8b1f57e
Kevin Jagodic <55063773+jkvn@users.noreply.github.com>
2023-12-28 03:32:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
+    let arraySize = 10000000; // 10 million elements
+    let largeArray = new Array(arraySize).fill().map(() => Math.random());
+    let sumOfElements = largeArray.reduce((acc, val) => acc + val, 0);
+
     res.send('Hello World!');
 });
 
